01d19e2a3873bd28d01e31617d3c0cf89d559845
ledesma90 <jledesma102@gmail.com>
2025-01-31 21:11:53 -0300

    Convertir app en una carpeta normal

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Comic_Neue, Lusitana  } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const comic_Neue = Comic_Neue({ weight:'400', subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
