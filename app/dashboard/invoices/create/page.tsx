7c6f969872ddcdf0008dff5c60677abf98bf9636
ledesma90 <jledesma102@gmail.com>
2025-02-02 22:33:18 -0300

    hola

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,6 +1,6 @@
 import Form from "@/app/ui/invoices/create-form";
-import { fetchCustomers } from "@/app/lib/data";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers } from "@/app/lib/data";
 
 export default async function Page() {
   const customers = await fetchCustomers();
